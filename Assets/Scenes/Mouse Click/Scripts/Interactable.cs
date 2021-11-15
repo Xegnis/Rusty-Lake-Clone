6b844d5f4de080d9d96eaf66d585899619408c8a
Xegnis <xegnis@gmail.com>
2021-11-15 03:02:10 -0500

    Ripple Effect and Sound Effect

---
+++ Assets/Scenes/Mouse Click/Scripts/Interactable.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+    void Awake()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        //do things
+    }
+}
