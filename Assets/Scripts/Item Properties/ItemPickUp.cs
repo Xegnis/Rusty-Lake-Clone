c6ffd5c1826bf4f2f05fbf89a21573fe8d58bab7
SouthRiverHypnos <rz2077@nyu.edu>
2021-12-08 13:21:56 -0500

    Interaction Update

--- Assets/Scripts/Item Properties/ItemPickUp.cs
+++ Assets/Scripts/Item Properties/ItemPickUp.cs
@@ -49,4 +49,11 @@ public class ItemPickUp : Interactable {
             Destroy(gameObject);
         }
     }
+
+    public void EnablePickUp()
+    {
+        if (!canPickUp) {
+            canPickUp = true;
+        }
+    }
 }
