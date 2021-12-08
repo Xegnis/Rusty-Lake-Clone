ad403d567d148fa6763d94923c27a1cfcb6e2869
VeronicaQu <tq374@nyu.edu>
2021-12-08 18:00:52 -0500

    error fix

---
+++ Assets/InventoryScripts/InventoryUI.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public Transform itemsParent;
+
+    Inventory inventory;
+
+   
+    List<InventorySlot> slots = new List<InventorySlot>();
+   
+    void Start()
+    {
+        inventory = Inventory.instance;
+        inventory.OnItemChangedCallback += UpdateUI;
+
+       // slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+       slots = new List<InventorySlot>(itemsParent.GetComponentsInChildren<InventorySlot>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateUI()
+    {
+        for(int i = 0; i < slots.Count; i++)
+        {
+            if( i < inventory.items.Count)
+            {
+                
+               slots[i].AddItem(inventory.items[i]);
+            } else 
+            {
+                slots[i].ClearSlot();
+            }
+
+        }
+        
+    }
+}
