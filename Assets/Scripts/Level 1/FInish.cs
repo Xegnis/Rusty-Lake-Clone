276e225e0b3540fd0800b7d821599be86de61c8e
VeronicaQu <tq374@nyu.edu>
2021-12-13 19:53:47 -0500

    FInish

---
+++ Assets/Scripts/Level 1/FInish.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FInish : MonoBehaviour
+{
+    public GameObject tree;
+
+    public void BigTree(){
+        tree.SetActive(true);
+    }
+}
